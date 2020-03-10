55e4d7c7c68d45b1e429cac6810c696b9e9f461a
Samrat Das <oozsober@gmail.com>
2020-03-10 17:09:19 +0530

    Deploying to rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,9 @@
 require('babel-register');
 require('babel-polyfill');
 
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "ship blast rival pause immense chalk echo wreck flower meat design camera";
+
 module.exports = {
   networks: {
     development: {
@@ -8,6 +11,14 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    rinkeby: {
+      provider: function() { 
+       return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/746654cf395f402da7da7820b704a1f7");
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+  }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
