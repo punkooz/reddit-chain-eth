924b32af6f1e9d3d928360feab5314af292e11bc
Samrat Das <oozsober@gmail.com>
2020-03-10 17:56:31 +0530

    Updates

---
+++ precache-manifest.f987a8fd3ff4d859a2ae42e48eafc9cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "509c0dbc85f453335ffc2808e9f8f70d",
+    "url": "/reddit-chain-eth/index.html"
+  },
+  {
+    "revision": "737c23818500e869f20d",
+    "url": "/reddit-chain-eth/static/css/2.85f22f3a.chunk.css"
+  },
+  {
+    "revision": "b82fc9255b13bfa28f9a",
+    "url": "/reddit-chain-eth/static/css/main.18de8aef.chunk.css"
+  },
+  {
+    "revision": "737c23818500e869f20d",
+    "url": "/reddit-chain-eth/static/js/2.2834ac66.chunk.js"
+  },
+  {
+    "revision": "bd48e7aad8ffbde0152bdbc9a20111c5",
+    "url": "/reddit-chain-eth/static/js/2.2834ac66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b82fc9255b13bfa28f9a",
+    "url": "/reddit-chain-eth/static/js/main.8963f2da.chunk.js"
+  },
+  {
+    "revision": "88d5840a33e6a0aed90e",
+    "url": "/reddit-chain-eth/static/js/runtime-main.29368538.js"
+  }
+]);
